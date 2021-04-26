bafe8fc0173071dd74cd9b87524b48c595765aec
Matt <matt.reimer@gmail.com>
2021-04-26 08:44:31 -0700

    updating the python script

--- python/gh_pages_publish.py
+++ python/gh_pages_publish.py
@@ -3,6 +3,9 @@ import glob
 import json
 import os
 import shutil
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 # These are the folders we will scrape
 FOLDERS = [
@@ -22,7 +25,9 @@ def md5(fname: str) -> str:
         with open(fname, "rb") as f:
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
-        return hash_md5.hexdigest()
+        dig = hash_md5.hexdigest()
+        logging.debug('   {} :: {}'.format(dig, fname))
+        return dig
     except Exception as e:
         print(e)
         return None
@@ -34,6 +39,7 @@ def build_index():
     """
     Create an index file with MD5s we can compare to
     """
+    logging.info('Building Index:')
     output = {}
     for folder in FOLDERS:
         files = glob.glob(os.path.join(
@@ -55,31 +61,43 @@ def build_index():
     with open(INDEX_JSON, 'w') as indf:
         json.dump(output, indf, sort_keys=True, indent=4)
 
+    logging.info('  complete.')
+
 
 def prep_copy():
     """
     Copy everything into a PUBLIC folder for deployment
     """
     if os.path.isdir(PUBLIC_DIR):
+        logging.warning('Folder exists: deleting {}'.format(PUBLIC_DIR))
         shutil.rmtree(PUBLIC_DIR)
 
     os.mkdir(PUBLIC_DIR)
 
     if os.path.isfile(INDEX_JSON):
-        shutil.copy(INDEX_JSON, os.path.join('.', PUBLIC_DIR, INDEX_JSON))
+        src = INDEX_JSON
+        dst = os.path.join('.', PUBLIC_DIR, INDEX_JSON)
+        shutil.copy(src, dst)
+        logging.info('Copied {} --> {}'.format(src, dst))
 
     if os.path.isfile(INDEX_HTML):
-        shutil.copy(INDEX_HTML, os.path.join('.', PUBLIC_DIR, INDEX_HTML))
+        src = INDEX_HTML
+        dst = os.path.join('.', PUBLIC_DIR, INDEX_HTML)
+        shutil.copy(src, dst)
+        logging.info('Copied {} --> {}'.format(src, dst))
 
     for folder in FOLDERS:
         src = os.path.join('.', folder)
         dst = os.path.join('.', PUBLIC_DIR, folder)
         shutil.copytree(src, dst)
+        logging.info('Copied {} --> {}'.format(src, dst))
 
     for fl in FILES:
         src = os.path.join('.', fl)
         dst = os.path.join('.', PUBLIC_DIR, fl)
         shutil.copy(src, dst)
+        logging.info('Copied {} --> {}'.format(src, dst))
+    logging.info('Copied {} individual files'.format(len(FILES)))
 
 
 if __name__ == '__main__':
