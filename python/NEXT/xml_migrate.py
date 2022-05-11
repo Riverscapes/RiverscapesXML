ad334593b58f21bda986e326e1e6a3fcffcdc825
Matt Reimer <matt@northarrowresearch.com>
2022-05-11 10:46:09 -0400

    WarehouseNEXT Schema

---
+++ python/NEXT/xml_migrate.py
@@ -0,0 +1,22 @@
+import argparse
+
+def migrate_xml_file(in_path:str, out_path:str) -> str:
+  """Safely translate from the old path to the new path
+
+  Args:
+      in_path (str): [description]
+
+  Returns:
+      str: [description]
+  """
+  return in_path + " hello"
+
+def main():
+  print('HELLO')
+  print (translate_path('I am '))
+  pass
+
+
+
+if __name__ == '__main__':
+  main()
