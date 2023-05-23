9106e7ca917f81760f121e79043149d99b008800
Matt Reimer <matt@northarrowresearch.com>
2023-05-23 10:31:09 -0400

    getting ready to package

--- python/packages/rsxml/setup.py
+++ python/packages/rsxml/setup.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
-# Notes; colored and matplotlib are optional
-
 import re
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # https://packaging.python.org/discussions/install-requires-vs-requirements/
 install_requires = [
-    'semver>=2.10.2', 'argparse'
+    'semver>=2.10.2',
+    'argparse'
 ]
 
 with open("README.md", "rb") as f:
@@ -18,18 +16,24 @@ version = re.search(
     re.M
 ).group(1)
 
-setup(name='rsxml',
-      version=version,
-      description='Riverscapes XML helpers for use across Python3 open-source GIS Stack',
-      author='Matt Reimer',
-      license='MIT',
-      python_requires='>3.10.0',
-      long_description=long_descr,
-      author_email='info@northarrowresearch.com',
-      install_requires=install_requires,
-      zip_safe=False,
-      url='https://github.com/Riverscapes/RiverscapesXML',
-      packages=[
-          'src',
-      ],
-      )
+setup(
+    name='rsxml',
+    version=version,
+    description='Riverscapes XML helpers for use across Python3 open-source GIS Stack',
+    author='Matt Reimer',
+    license='MIT',
+    python_requires='>=3.10.0',
+    long_description=long_descr,
+    author_email='info@northarrowresearch.com',
+    install_requires=install_requires,
+    url='https://github.com/Riverscapes/RiverscapesXML',
+    packages=find_packages('src'),  # Find packages under the 'src' directory
+    package_dir={'': 'src'},  # Specify that the package code is under 'src'
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        # Add more classifiers relevant to your package
+    ],
+)
